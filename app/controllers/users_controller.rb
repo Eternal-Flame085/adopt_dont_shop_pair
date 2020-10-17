4c8126e554ecfa223eb406205b7532dda33315be
Roberto Basulto <roberto.basulto085@gmail.com>
2020-10-16 21:39:14 -0400

    Finished user story 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = Review.where("user_id = #{@user.id} ")
+    best_rating = Review.best_rating(@reviews)
+    worst_rating = Review.worst_rating(@reviews)
+    @highlighted_reviews = []
+
+    if !best_rating.nil? || !worst_rating.nil?
+      @highlighted_reviews << best_rating
+      @highlighted_reviews << worst_rating
+    end
   end
 
   def create
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -32,5 +32,43 @@ describe 'As a visitor' do
     expect(page).to have_content("Rating: #{review_1.rating}")
     expect(page).to have_content("Content: #{review_1.content}")
     end
+
+    it 'See a section for Highlighted Reviews' do
+      user = User.create(name: 'Mike Dao',
+                           address: '6254',
+                           city: 'Miami',
+                           state: 'CH',
+                           zip: '636')
+      shelter_1 = Shelter.create(name: 'AOA',
+                               address: '6254',
+                               city: 'Denver',
+                               state: 'CH',
+                               zip: '636')
+      shelter_2 = Shelter.create(name: 'MIA',
+                               address: '6254',
+                               city: 'Miami',
+                               state: 'CH',
+                               zip: '636')
+      review_1 = Review.create(title: 'Best Shelter', rating: '10', content: 'Test Info',
+                               photo: '', user_name: 'Mike Dao',
+                               shelter_id: "#{shelter_1.id}", user_id: "#{user.id}")
+      review_2 = Review.create(title: 'Worst Shelter', rating: '1', content: 'Test Info',
+                               photo: '', user_name: 'Mike Dao',
+                               shelter_id: "#{shelter_2.id}", user_id: "#{user.id}")
+
+      visit "/users/#{user.id}"
+      
+      within '#highlighted-reviews' do
+        expect(page).to have_content("Highlighted Reviews")
+
+        expect(page).to have_content("Title: #{review_1.title}")
+        expect(page).to have_content("Rating: #{review_1.rating}")
+        expect(page).to have_content("Content: #{review_1.content}")
+
+        expect(page).to have_content("Title: #{review_2.title}")
+        expect(page).to have_content("Rating: #{review_2.rating}")
+        expect(page).to have_content("Content: #{review_2.content}")
+      end
+    end
   end
 end
